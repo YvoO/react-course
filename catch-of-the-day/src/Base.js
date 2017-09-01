c71a53ef0c2977f6f8b28ace4fd387d3924d89e7
Yvo <y.vanoosterum@gmail.com>
2017-09-01 11:12:35 +0200

    added bunch of stuff

---
+++ catch-of-the-day/src/Base.js
@@ -0,0 +1,9 @@
+import Rebase from 're-base';
+
+const Base = Rebase.createClass({
+	apiKey: "AIzaSyCqoOGU3zr94916en680QqQclFFomrv3Gk",
+    authDomain: "catch-of-the-day-yvo.firebaseapp.com",
+    databaseURL: "https://catch-of-the-day-yvo.firebaseio.com"
+});
+
+export default Base;
--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -4,18 +4,50 @@ import Order from './Order';
 import Inventory from './Inventory';
 import sampleFishes from '../sample-fishes';
 import Fish from './Fish';
+import Base from '../Base';
 
 export default class App extends React.Component {
 	constructor(){
 		super();
 		this.addFish = this.addFish.bind(this);
+		this.updateFish = this.updateFish.bind(this);
+		this.removeFish = this.removeFish.bind(this);
 		this.loadSamples = this.loadSamples.bind(this);
 		this.addToOrder = this.addToOrder.bind(this);
+		this.removeFromOrder = this.removeFromOrder.bind(this);
 		//getinitialState
 		this.state = {
 			fishes: {},
 			order: {},
 		};
+		console.log(this.state.fishes);
+		console.log(this.state.order);
+	}
+
+	componentWillMount() {
+		// this runs right before the <App> is rendered
+		this.ref = Base.syncState(`${this.props.params.storeID}/fishes`, 
+		{
+			context: this,
+			state: 'fishes'
+		});
+
+		// check if there is any order in local storage
+		const localStorageRef = localStorage.getItem(`order-${this.props.params.storeID}`);
+
+		if(localStorageRef){
+			this.setState({
+				order: JSON.parse(localStorageRef)
+			})
+		}
+	}
+
+	componentWillUnmount() {
+		Base.removeBinding(this.ref); 
+	}
+
+	componentWillUpdate(nextProps, nextState){
+		localStorage.setItem(`order-${this.props.params.storeID}`, JSON.stringify(nextState.order));
 	}
 
 	addFish(fish){
@@ -27,6 +59,22 @@ export default class App extends React.Component {
 		//set state/
 		this.setState({
 			fishes: fishes 
+		})
+	}
+
+	updateFish(key, updatedFish){
+		const fishes = {...this.state.fishes};
+		fishes[key] = updatedFish;
+		this.setState({
+			fishes: fishes
+		})
+	}
+
+	removeFish(key){
+		const fishes = {...this.state.fishes}
+		fishes[key] = null;
+		this.setState({
+			fishes: fishes
 		})
 	}
 
@@ -47,6 +95,14 @@ export default class App extends React.Component {
 		})
 	}
 
+	removeFromOrder(key){
+		const order = {...this.state.order};
+		delete order[key];
+		this.setState({
+			order: order
+		})
+	}
+
 	render() {
 		return (
 			<div className="catch-of-the-day">
@@ -60,8 +116,19 @@ export default class App extends React.Component {
 						}
 					</ul>
 				</div>
-				<Order fishes={this.state.fishes} order={this.state.order}/>
-				<Inventory addFish={this.addFish} loadSamples={this.loadSamples}/>
+				<Order 
+					fishes={this.state.fishes} 
+					order={this.state.order}
+					params={this.props.params}
+					removeFromOrder={this.removeFromOrder}
+				/>
+				<Inventory 
+					fishes={this.state.fishes}
+					addFish={this.addFish} 
+					updatedFish={this.updateFish} 
+					removeFish={this.removeFish}
+					loadSamples={this.loadSamples}
+				/>
 			</div>
 		)
 	}
